232dde50f681c70e6b14bacec3e7b4d06464194b
ZeZhan <66437581+ZeZhan@users.noreply.github.com>
2022-01-04 11:54:50 -0800

    Add files via upload
    current reconstructed casadi job

--- casadi_reconstruct/optimizer_casadi.py
+++ casadi_reconstruct/optimizer_casadi.py
@@ -83,14 +83,18 @@ class MPC_car():
 
     def object_func(self, states, controls, reference_states, horizon):
         obj = 0 
-        Q = np.array([[5.0, 0.0, 0.0, 0.0, 0.0],[0.0, 5.0, 0.0, 0.0, 0.0],[0.0, 0.0, 100, 0.0, 0.0], [0.0, 0.0, 0.0, 1, 0.0], [0.0, 0.0, 0.0, 0.0, 1]])
-        R = np.array([[1, 0.0], [0.0, 1]])
+        #states penalty
+        Q = np.array([[5.0, 0.0, 0.0, 0.0, 0.0],[0.0, 5.0, 0.0, 0.0, 0.0],[0.0, 0.0, 200, 0.0, 0.0], [0.0, 0.0, 0.0, 1, 0.0], [0.0, 0.0, 0.0, 0.0, 80]])
+        #controls penalty
+        R = np.array([[0.1, 0.0], [0.0, 0.1]])
+        #end term penalty
+        P = np.array([[5.0, 0.0, 0.0, 0.0, 0.0],[0.0, 5.0, 0.0, 0.0, 0.0],[0.0, 0.0, 200, 0.0, 0.0], [0.0, 0.0, 0.0, 1, 0.0], [0.0, 0.0, 0.0, 0.0, 80]])
         ## cost
         for i in range(horizon):
             # obj = obj + ca.mtimes([(X[:, i]-P[3:]).T, Q, X[:, i]-P[3:]]) + ca.mtimes([U[:, i].T, R, U[:, i]])
     
             obj = obj + (states[:, i]-reference_states[5:]).T @ Q @ (states[:, i]-reference_states[5:]) + controls[:, i].T @ R @ controls[:, i] 
-            + (states[:, -1]-reference_states[5:]).T @ Q @ (states[:, -1]-reference_states[5:])
+            + (states[:, -1]-reference_states[5:]).T @ P @ (states[:, -1]-reference_states[5:])
         return obj
 
     def equal_constraints(self, states, horizon):
